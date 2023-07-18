5d0ca1214afcc32ad4d2bb8716002984ec2bc2e7
Goodstar Ohia <goodstarohia@gmail.com>
2023-07-18 23:32:14 +0100

    added new lines of code

--- drill/Computation drill(Chapter 4)/main.cpp
+++ drill/Computation drill(Chapter 4)/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 /**
@@ -35,10 +36,14 @@ using namespace std;
 //         int a, b;
 
 //         while (cin >> a >> b){
-//                 if (a < b)
-//                 cout << "the smaller value is: " << a << endl;
-//                 else
-//                 cout << "The larger value is: " << b << endl;
+//                 if (a < b) {
+//                         cout << "The smaller value is: " << a << endl;
+//                         cout << "The larger value is: " << b << endl;
+//                 }
+//                 else if (b < a) {
+//                         cout << "The smaller value is: " << b << endl;
+//                         cout << "The larger value is: " << a << endl;
+//                 }
 //         }
 //         return 0;
 // }
@@ -50,17 +55,84 @@ using namespace std;
  * 
  * Return 0;
 */
+// int main()
+// {
+//         int a, b;
+
+//         while (cin >> a >> b){
+//                 if (a < b) {
+//                         cout << "The smaller value is: " << a << endl;
+//                         cout << "The larger value is: " << b << endl;
+//                 }
+//                 else if (b < a) {
+//                         cout << "The smaller value is: " << b << endl;
+//                         cout << "The larger value is: " << a << endl;
+//                 }
+//                 else
+//                 cout << "The numbers are equal" << endl;
+//         }
+//         return 0;
+// }
+
+/**
+ * main - modify third drill
+ * Description: Change the object type from int to double
+ * 
+ * Return: 0 always success
+*/
+// int main()
+// {
+//         double a, b;
+
+//         while (cin >> a >> b){
+//                 if (a < b) {
+//                         cout << "The smaller value is: " << a << endl;
+//                         cout << "The larger value is: " << b << endl;
+//                 }
+//                 else if (b < a) {
+//                         cout << "The smaller value is: " << b << endl;
+//                         cout << "The larger value is: " << a << endl;
+//                         if (b == a - formula) {
+//                         cout << "The numbers are almost equal\n";
+//                         }
+//                 }
+//                 else
+//                 cout << "The numbers are equal" << endl;
+//         }
+//         return 0;
+// }
+
+/**
+ * main - modify fourth drill
+ * Description: Write out 'The numbers are almost equal' after writing ..
+ * .. out which is the smaller and larger if the two numbers differ by ..
+ * .. less than 1.0/100
+ * 
+ * Return: 0  
+*/
 int main()
 {
-        int a, b;
+        double a, b;
+        const double formula = 1.0 / 100;
 
         while (cin >> a >> b){
-                if (a < b)
-                cout << "The smaller value is: " << a << endl;
-                else if (a == b)
-                cout << "The numbers are equal" << endl;
+                if (a < b) {
+                        cout << "The smaller value is: " << fixed << setprecision(2) << a << endl;
+                        cout << "The larger value is: " << fixed << setprecision(2) << b << endl;
+                        if (a == b - formula) {
+                                cout << "The numbers are almost equal\n";
+                        }
+                        
+                }
+                else if (b < a) {
+                        cout << "The smaller value is: " << fixed << setprecision(2) << b << endl;
+                        cout << "The larger value is: " << fixed << setprecision(2) << a << endl;
+                        if (b == a - formula) {
+                                cout << "The numbers are almost equal\n";
+                        }
+                }
                 else
-                cout << "The larger value is: " << b << endl;
+                cout << "The numbers are equal" << endl;
         }
         return 0;
 }
