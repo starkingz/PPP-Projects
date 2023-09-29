c25226a3800604180377f32111651cb51cf975e6
Goodstar Ohia <goodstarohia@gmail.com>
2023-09-29 21:33:06 +0100

    updated code

--- cppExcercises/computationExcercise/8.cpp
+++ cppExcercises/computationExcercise/8.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 int main()
 {
+        bool playagain {yes};
         vector <int> comp_vals {1, 2, 1, 1, 3, 1, 2, 3, 3, 1, 3};
         cout << "ROCK [1], PAPER [2], SCISSOR [3] GAMEPLAY\n";
         // cout << "Rock, paper, scissor shoot: " << endl;
         int val;
         int len = comp_vals.size();
 
+        if (playagain == yes) {
         for (int i = 0; i < len; i++) {
                 cout << "Rock, paper, scissor shoot: " << endl;
                 cin >> val;
@@ -19,39 +21,57 @@ int main()
                         case 1:
                         switch (comp_vals[i]) {
                                 case 1:
-                                cout << "Draw.\n";
+                                cout << "You play: ROCK\n"
+                                     << "Computer plays: ROCK\n"
+                                     << "Draw.\n";
                                 break;
                                 case 2:
-                                cout << "Computer wins.\n";
+                                cout << "You play: ROCK\n"
+                                     << "Computer plays: PAPER"
+                                     << "Computer wins.\n";
                                 break;
                                 default:
-                                cout << "You win.\n";
+                                cout << "You play: ROCK\n"
+                                     << "Computer plays: SCISSORS\n"
+                                     << "You win.\n";
                                 break;
                         }
                         break;
                         case 2:
                         switch (comp_vals[i]) {
                                 case 1:
-                                cout << "You win.\n";
+                                cout << "You play: PAPER\n"
+                                     << "Computer plays: ROCK\n"
+                                     << "You win.\n";
                                 break;
                                 case 2:
-                                cout << "Draw.\n";
+                                cout << "You play: PAPER\n"
+                                     << "Computer plays: PAPER"
+                                     << "Draw.\n";
                                 break;
                                 default:
-                                cout << "Computer wins.\n";
+                                cout << "You play: PAPER\n"
+                                     << "Computer plays: SCISSORS\n"
+                                     << "Computer wins.\n";
                                 break;
                         }
                         break;
                         case 3:
                         switch (comp_vals[i]) {
                                 case 1:
-                                cout << "Computer wins.\n";
+                                cout << "You play: SCISSORS\n"
+                                     << "Computer plays: ROCK\n"
+                                     << "Computer wins.\n";
                                 break;
                                 case 2:
-                                cout << "You win.\n";
+                                cout << "You play: SCISSORS\n"
+                                     << "Computer plays: PAPER\n"
+                                     << "You win.\n";
                                 break;
                                 case 3:
-                                cout << "Draw.\n";
+                                cout << "You play: SCISSORS\n"
+                                     << "Computer plays: SCISSORS\n"
+                                     << "Draw.\n";
                                 break;
                         }
                         break;
@@ -59,6 +79,16 @@ int main()
                         cout << "Invalid value.\n";
                         break;
                 }
-                cout << "Play again.\n";
+                cout << "Do you want to play again? (yes/no)\n";
+                cin >> playagain;
+                switch (i) {
+                        case len:
+                        cout << "Game Over.\n";
+                        break;
+                }
+        }
+        }
+        else {
+                cout << "Game Over.\n";
         }
 }
