2288a29540cbc19cfd8de7ea068d2b356e9be1c6
Goodstar Ohia <goodstarohia@gmail.com>
2023-09-30 20:36:48 +0100

    update

--- cppExcercises/computationExcercise/9.cpp
+++ cppExcercises/computationExcercise/9.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 int get_prime(int n)
 {
-        if ((n % 2 == 1 && n % 3 == 1) || n == 2 || n == 3)
-        return n;
+        int val {0};
+        int num;
+        // int i = 0;
+
+        if (n == 2 || n == 3 || n == 5)
+        val = 1;
+        
+        if ((n % 2 == 1 && n % 3 == 1) || (n % 5 == 1 && n % 2 == 1)) {
+                val = 1;
+        }
+        if (val == 1) {
+                num = n;
+        }
+
+        return num;
+
+
 }
 
 int main()
@@ -17,13 +32,15 @@ int main()
         int prime {0};
         
         for (i = 1; i <= 100; i++) {
-                prime = get_prime(i);
-                primes.push_back(prime);
+                // if (i > 1) {
+                        prime = get_prime(i);
+                        primes.push_back(prime);
+                // }
         }
 
         len = primes.size();
-        for (j = 0; j < len; ++j) {
-                cout << "primes[" << i << "] == " << primes[i] << endl;
+        for (j = 1; j <= len; ++j) {
+                cout << "primes[" << j << "] == " << primes[j] << endl;
         }
 
         return 0;
