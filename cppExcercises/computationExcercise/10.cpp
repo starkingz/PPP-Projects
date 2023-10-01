f3b7785a3d8d5c55039ec3a72c5bf9ce21fd58ba
Goodstar Ohia <goodstarohia@gmail.com>
2023-10-01 06:12:32 +0100

    excercise 12

---
+++ cppExcercises/computationExcercise/10.cpp
@@ -0,0 +1,59 @@
+// Chapter 4 - Computation
+// Excercise 12
+// Date: 01/10/2023
+// Modification of excercise 11
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+/**
+ * main - compute prime numbers
+ * 
+ * Return: either true or false
+*/
+bool get_prime(int n)
+{
+        int i;
+
+        // not greater 1
+        if (n <= 1)
+        return false;
+
+        // start from 2
+        for (i = 2; i <= n / 2; ++i) {
+                if (n % i == 0) // check if n is divisible
+                return false;
+        }
+        return true;
+
+}
+
+/**
+ * main - read a positive number and print the primes
+*/
+int main()
+{
+        vector <int> primes;
+        int i, j, len;
+        primes.push_back(2); // Put the smallest prime into vector
+
+        cout << "Enter maximum number: ";
+        int max {0};
+        cin >> max;
+
+        // start from 3 to max [3:max]
+        for (i = 3; i <= max; i++) {
+                        if (get_prime(i))
+                        primes.push_back(i); // put i into vector
+        }
+
+        len = primes.size(); // size of vector
+        cout << "Primes between 1 and 100: \n";
+        for (j = 0; j < len; ++j) {
+                cout << primes[j] << endl;
+        }
+
+        return 0;
+}
