264b9ebaacdcee7fb31b8514f35ee49fd1549ae2
Goodstar Ohia <goodstarohia@gmail.com>
2023-10-07 16:56:29 +0100

    update

--- cppExcercises/computationExcercise/16.cpp
+++ cppExcercises/computationExcercise/16.cpp
@@ -46,9 +46,8 @@ int main()
         double x2 {0.0};
         double discriminant {(square(b) - (4 * a * c))};
 
-        x1 = (-b + sqroot(square(b) - (4 * a * c))) / (2 * a);
-        x2 = (-b - sqroot(square(b) - (4 * a * c))) / (2 * a);
-        // cout << discriminant << endl;
+        x1 = -b + sqroot(discriminant) / (2 * a);
+        x2 = -b - sqroot(discriminant) / (2 * a);
 
         if (discriminant < 0) {
                 x1 = -b / (2 * a);
