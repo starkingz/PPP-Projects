fb9927fb0bb2322379b9bbd630c9dfabb8cac1e3
Goodstar Ohia <goodstarohia@gmail.com>
2023-12-21 20:02:26 +0100

    add exception to catch errors

--- cppExcercises/trythis_15.cpp
+++ cppExcercises/trythis_15.cpp
@@ -1,14 +1,18 @@
 #include "../std_lib_facilities.h"
 
 /**
- * main - find the highest lowest and average of given temperatures
+ * main - find the highest, lowest and average of given temperatures
 */
 int main()
-{
+try {
         vector <double> temps;
 
-        for (double temp; cin >> temp;)
+        
+        for (double temp; cin >> temp;) {
                 temps.push_back(temp);
+        }
+        if(!cin)
+                error("Could'nt read a double from 'main()'");
 
         double sum {0};
         double high_temp {0};
@@ -21,7 +25,7 @@ int main()
                         high_temp = x;
                         low_temp = x;
                 }
-                if (x > high_temp) // chrck for highest temp
+                if (x > high_temp) // check for highest temp
                         high_temp = x;
                 if (x < low_temp) // check for lowest temp
                         low_temp = x;
@@ -35,4 +39,18 @@ int main()
         cout << "Average temperature: " << sum / len << '\n';
 
         return 0;
+}
+catch (runtime_error& e) {
+        cerr << e.what() << endl;
+        
+        cout << "Enter '~' to terminate program\n";
+        for (char ch; cin >> ch;) {
+                if(ch == '~')
+                return 1;
+        }
+        return 1;
+}
+catch (...) {
+        cerr << "Something went wrong\n";
+        return 1;
 }
