aa89a53b391cf286b01f944d2722a0940619ac19
Goodstar Ohia <goodstarohia@gmail.com>
2024-01-26 02:58:33 +0100

    testing token - a user defined type

--- writing_a_program_test.cpp
+++ writing_a_program_test.cpp
@@ -39,53 +39,82 @@
  * 
  * Return: Always 0
 */
-int main()
-try
-{
-        cout << "Please enter expression (we can handle +, -, * and /)\n";
-        cout << "Add x to end expression: ";
-        int lval {0};
-        cin >> lval;
-        if (!cin)
-                error("No first operand");
-        int rval;
+// int main()
+// try
+// {
+//         cout << "Please enter expression (we can handle +, -, * and /)\n";
+//         cout << "Add x to end expression: ";
+//         int lval {0};
+//         cin >> lval;
+//         if (!cin)
+//                 error("No first operand");
+//         int rval;
 
-        for (char op; cin >> op;)
-        {
-                if (op != 'x')
-                        cin >> rval;
-                if (!cin)
-                        error("No second operand");
+//         for (char op; cin >> op;)
+//         {
+//                 if (op != 'x')
+//                         cin >> rval;
+//                 if (!cin)
+//                         error("No second operand");
                 
-                switch (op)
-                {
-                        case '+':
-                        lval += rval;
-                        break;
-                        case '-':
-                        lval -= rval;
-                        break;
-                        case '*':
-                        lval *= rval;
-                        break;
-                        case '/':
-                        lval /= rval;
-                        break;
-                        default:
-                        cout << "Result: " << lval << endl;
-                        cout << "===== END OF EXPRESSION =====\n";
-                        keep_window_open("~");
-                        return 0;
-                }
-        }
-}
-catch (runtime_error& e) {
-        cerr << e.what() << endl;
-        keep_window_open("~");
-        return 1;
-}
-catch (...) {
-        cerr << "Something went wrong\n";\
-        keep_window_open("~");
-        return 2;
+//                 switch (op)
+//                 {
+//                         case '+':
+//                         lval += rval;
+//                         break;
+//                         case '-':
+//                         lval -= rval;
+//                         break;
+//                         case '*':
+//                         lval *= rval;
+//                         break;
+//                         case '/':
+//                         lval /= rval;
+//                         break;
+//                         default:
+//                         cout << "Result: " << lval << endl;
+//                         cout << "===== END OF EXPRESSION =====\n";
+//                         keep_window_open("~");
+//                         return 0;
+//                 }
+//         }
+// }
+// catch (runtime_error& e) {
+//         cerr << e.what() << endl;
+//         keep_window_open("~");
+//         return 1;
+// }
+// catch (...) {
+//         cerr << "Something went wrong\n";\
+//         keep_window_open("~");
+//         return 2;
+// }
+
+/**
+ * main - check how Tokens work
+ * 
+ * Return: Always 0
+*/
+int main()
+{
+        class Token {
+                public:
+                        char kind;
+                        double value;
+        };
+
+        Token t;
+        t.kind = '+';
+
+        Token t2;
+        t2.kind = '8';
+        t2.value = 3.14;
+
+        Token tt = t;
+
+        if (tt.kind != t.kind)
+                error("impossible");
+        t = t2;
+
+        cout << t.value << endl;
 }
