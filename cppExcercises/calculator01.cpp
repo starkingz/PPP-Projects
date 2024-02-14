c59e5976dc0ad4d823378bd942501414a28e0fa8
Goodstar Ohia <goodstarohia@gmail.com>
2024-02-14 04:07:37 +0100

    update

--- cppExcercises/calculator01.cpp
+++ cppExcercises/calculator01.cpp
@@ -17,34 +17,6 @@ public:
         :kind(ch), value(val) { }
 };
 
-//------------------------------------------------------------------------------
-/*
-Token get_token()    // read a token from cin
-{
-    char ch;
-    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
-
-    switch (ch) {
- //not yet   case ';':    // for "print"
- //not yet   case 'q':    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/': 
-        return Token(ch);        // let each character represent itself
-    case '.':
-    case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8': case '9':
-        {    
-            cin.putback(ch);         // put digit back into the input stream
-            double val;
-            cin >> val;              // read a floating-point number
-            return Token('8',val);   // let '8' represent "a number"
-        }
-    default:
-        error("Bad token");
-    }
-}
-*/
-//------------------------------------------------------------------------------
-
 class Token_stream {
     public:
         Token get();            // get a Token
