a566fb581790df1e5113f01af78512db42f0b57e
Goodstar Ohia <goodstarohia@gmail.com>
2024-03-14 03:19:59 +0100

    v1.1 - add new features and code fix

--- cppExcercises/writingAProgram/3.cpp
+++ cppExcercises/writingAProgram/3.cpp
@@ -17,22 +17,33 @@ vector <Name_value> ns;
 
 Name_value get()
 {
+        int len;
         string n;
         cin >> n;
         int v;
         cin >> v;
+        if (!cin)
+                error("Couldn't read the input");
 
+        len = ns.size();
+        for (int i = 0; i < len; i++)
+        {
+                if (n == ns[i].name)
+                        error("Name already exists");
+        }
         return Name_value(n, v);
 }
 
 int main()
-{
+try {
         int len;
 
         while (cin)
         {
                 Name_value v = get();
                 len = ns.size();
+                if (v.name == "quit" || v.name == "q")
+                        break;
                 if (v.name == "NoName" && v.value == 0)
                 {
                         for (int i = 0; i < len; i++)
@@ -40,9 +51,20 @@ int main()
                                 cout << ns[i].name << endl
                                      << ns[i].value << endl;
                         }
-                        break;
                 }
-                ns.push_back(v);       // put cin into vector
+                else
+                        ns.push_back(v);       // put cin into vector
         }
+        keep_window_open();       
         return 0;
+}
+catch (exception& e) {
+        cerr << "error: " << e.what() << endl;
+        keep_window_open();
+        return 2;
+}
+catch (...) {
+        cerr << "Something went wrong\n";
+        keep_window_open();
+        return 3;
 }
