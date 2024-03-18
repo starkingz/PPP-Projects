9d14a8164f953d4c29d6ab01066abb148ceb3e14
Goodstar Ohia <goodstarohia@gmail.com>
2024-03-18 04:07:48 +0100

    add init() and is_sentence()

--- cppExcercises/writingAProgram/5.cpp
+++ cppExcercises/writingAProgram/5.cpp
@@ -4,8 +4,48 @@
 
 #include "../../std_lib_facilities.h"
 
+vector <string> noun;
+vector <string> verb;
+vector <string> conj;
+
+void init()
+{
+        // put words in noun
+        noun.push_back("birds");
+        noun.push_back("fish");
+        noun.push_back("c++");
+
+        // put words in verb
+        verb.push_back("rules");
+        verb.push_back("fly");
+        verb.push_back("swim");
+
+        // put words into conj
+        conj.push_back("and");
+        conj.push_back("or");
+        conj.push_back("but");
+
+}
+
+bool is_sentence()
+{
+        if (!is_noun())
+                return false;
+        if (!is_verb())
+                return false;
+
+        string dot;
+        cin >> dot;
+        if (dot == ".")
+                return true;
+        if (!is_conj())
+                return false;
+        // return true;
+}
+
 int main()
 {
+        init();
         string sen;
         bool ok {false};
 
