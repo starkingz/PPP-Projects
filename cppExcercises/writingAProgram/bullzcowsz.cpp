1db0a55f5678883e461ae8e498c6366ce28839f4
Goodstar Ohia <goodstarohia@gmail.com>
2024-03-26 19:11:58 +0100

    add init() to store computer data into vector

--- cppExcercises/writingAProgram/bullzcowsz.cpp
+++ cppExcercises/writingAProgram/bullzcowsz.cpp
@@ -2,7 +2,7 @@
 // Exercise 8
 // Date: 26.03.2024
 
-
+//Making a prototype of the program
 #include "../../std_lib_facilities.h" //external header file
 
 // read and store two sequence of ints 
@@ -17,3 +17,18 @@ class Game_counts
                 {
                 }
 };
+
+vector <char> comg;     // store computer guesses
+
+/**
+ * init - put computer guesses into comg vector
+ * 
+ * Return: void
+*/
+void init()
+{
+        comg.push_back('a');
+        comg.push_back('b');
+        comg.push_back('c');
+        comg.push_back('d');
+}
