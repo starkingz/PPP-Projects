e6c9fa3a149d55a4710272296548ed06f4aa933a
Goodstar Ohia <goodstarohia@gmail.com>
2024-03-27 06:21:18 +0100

    add second() to read second input from istream

--- drill/writing_a_program drill (chapter 6)/calculator02buggy.cpp
+++ drill/writing_a_program drill (chapter 6)/calculator02buggy.cpp
@@ -35,9 +35,13 @@ public:
 
 class Token_stream {
 public:
-    Token_stream();   // make a Token_stream that reads from cin
+    // Token_stream();   // make a Token_stream that reads from cin
     Token get();      // get a Token (get() is defined elsewhere)
     void putback(Token t);    // put a Token back
+
+    Token_stream()
+        :full(false), buffer(0) {}
+
 private:
     bool full;        // is there a Token in the buffer?
     Token buffer;     // here is where we keep a Token put back using putback()
