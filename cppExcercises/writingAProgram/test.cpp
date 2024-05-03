9a10174085cb9f9c43c205a13617c6f70765cee9
starkingz <goodstarohia@gmail.com>
2024-05-03 01:28:39 +0000

    add more testing

--- cppExcercises/writingAProgram/test.cpp
+++ cppExcercises/writingAProgram/test.cpp
@@ -4,19 +4,47 @@
 
 int main()
 {
-	cout << "Enter a number of factorise: ";
-	int n;
-	cin >> n;
+	cout << "Enter character to close the program\n";
+	string s;
+	cin >> s;
 
-	double res = 1;
-
-	for (int i = 1; i <= n; i++)
+	while (true)
 	{
+		cout << "Please enter " << s << " to exit program\n";
+		string ss;
 
-		res *= i;
-		cout << i << ". " << res << endl;
+		while (cin >> ss && ss != s)
+			cout << "Please enter " << s << " to exit program\n";
+		return 1;
 	}
-	cout << res << endl;
+	return 1;
+	// cout << "Enter a number of factorise: ";
+	// int n;
+	// cin >> n;
+
+	// long long int res = 1;
+	// long int res1 = 1;
+	// int res2 = 1;
+	// double res3 = 1;
+	// float res4 = 1;
+	// cout << "factorise(" << n << ")" <<  endl;
+	// for (int i = 1; i <= n; i++)
+	// {
+
+	// 	res *= i;
+	// 	res1 *= i;
+	// 	res2 *= i;
+	// 	res3 *= i;
+	// 	res4 *= i;
+	// 	cout << i << ". long long: " << res << ", long int: " << res1
+	// 	     << ", int: " << res2 << ", double: "<< res3 << ", flaot: " << res4 << endl;
+	// }
+	// cout << res << endl;
+	// cout << "Size of long long int: " << sizeof(long long int) << endl;
+	// cout << "Size of long long int variable: " << sizeof(res) << endl;
+	// cout << "Size of long int: " << sizeof(long int) << endl;
+	// cout << "Size of long int variable: " << sizeof(res1) << endl;
+	// cout << "Size of double: " << sizeof(double) << endl;
 	// cout << "Enter two numbers to multiply: ";
 	// int a = 0, b = 0;
 	// cin >> a >> b;
@@ -27,5 +55,5 @@ int main()
 	// if (mul_val <= 0)
 	// 	error("Overflow");
 
-	return 0;
+	// return 0;
 }
