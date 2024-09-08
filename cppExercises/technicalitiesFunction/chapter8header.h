5d3fad9750cb4e0cd90f62c19179595f6d99ed2e
starkingz <goodstarohia@gmail.com>
2024-09-08 23:32:09 +0000

    add header file for chapter 8

---
+++ cppExercises/technicalitiesFunction/chapter8header.h
@@ -0,0 +1,10 @@
+#include "../../std_lib_facilities.h"
+
+/**
+ * print - print a vector of ints to output
+ * @v: store sequence of ints
+ * @s: label output
+ *
+ * Return: nothing
+ */
+void print(vector <int> v, std::string s);
